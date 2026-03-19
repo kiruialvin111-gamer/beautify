45406c400901da9126649a23f79824a701ebcde8
kiruialvin111 <kiruialvin111@gmail.com>
2026-03-19 13:56:45 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router ,Routes,Route,Link} from 'react-router-dom'
+import Signin from './Components/Signin';
+import Signup from './Components/Signup';
+import Addproducts from './Components/Addproducts'
+import Getproducts from './Components/Getproducts'
+import Mpesapayments from './Components/Mpesapayments';
+
+
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Beautify Cosmetics 💄</h1>
+      </header>
+      <nav>
+      <Link to='/signup' className='btn btn-success text-white m-4 btn-outline-info'>SignUp</Link>
+      <Link to='/signin' className='btn btn-success text-white m-4 btn-outline-info'>Signin</Link>
+      <Link to='/' className='btn btn-success text-white m-4 btn-outline-info'>Get Products</Link>
+       <Link to='/Addproducts' className='btn btn-success text-white m-4 btn-outline-info'>Add Products</Link>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Getproducts/>}/>
+        <Route path='/addproducts' element={<Addproducts/>}/>
+        <Route path='/mpesa' element={<Mpesapayments/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+      </Routes>
+    </div>
+      </Router>
+  );
+}
+
+export default App;
---
+++ src/Components/Getproducts.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+
+
+const Getproducts = () => {
+
+// initializing your hooks
+  
+  const[loading,SetLoading]=useState("")
+  const[error,SetError]=useState("")
+  const[products,SetProducts]=useState([])
+  const navigate=useNavigate()
+
+
+// a variable to store our images
+  const img_url="http://kiruialvin.alwaysdata.net/static/images/"
+
+  // a function to fetch or get products from the api
+  const fetchproducts=async()=>{
+
+  try {
+    SetLoading("please wait as we retrieve your products")
+
+    // call your Api
+
+    const response=await axios.get("http://kiruialvin.alwaysdata.net/api/getproductdetails")
+
+
+    SetProducts(response.data)
+    console.log("The response is",response)
+    SetLoading("")
+    
+  } catch (error) {
+    SetLoading("")
+    SetError(error.message)
+
+  }
+  }
+  // end of the fetch function where we call our useEffect
+
+  useEffect(()=>{
+    fetchproducts()
+  },[])
+
+  
+  return (
+    <div className='row'> 
+      
+      <h1>Welcome to Beautify 💄</h1>
+      <p>glow like never before {"\u2728"}</p>
+      <p>Your one-stop shop for cosmetics</p>
+
+     
+      <br /><br />
+    
+
+      <p className='text-warning'>{loading}</p>
+      <p className='text-danger'>{error}</p>
+{/*  mapping through the products */}
+      {products.map((product)=>(
+
+      
+
+
+
+
+      <div className='col-md-3 justify-content-center '>
+        <div className='card shadow mt-3  '>
+          <img src={img_url + product.product_photo } alt="" className='product_img'/>
+
+          <div className='card-body'>
+            <h3 className='text-success'>{product.product_name}</h3>
+            <p className='text-secondary'>{product.product_description}</p>
+            <b className='text-info'>{product.product_cost}</b>
+
+
+            <br />
+            <input 
+                type="button" 
+         
+              value="Shop Now 🛒 "
+              className="btn btn-success w-100"
+                 onClick={() => navigate("/mpesa",{ state: { product } })}/>
+                  
+        
+        
+    
+          </div>
+
+        </div>
+
+      </div>
+      
+
+        
+    ))}
+    </div>
+  )
+}
+
+
+export default Getproducts
